9546e2b3cd97a8eb3858ec6976a74f36c6527df0
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-18 00:40:46 +0100

    Adding python scripts for task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+11. Where can I learn Python?
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns a list of schools having a particular topic
+    """
+    return [
+        school for school in mongo_collection.find() if topic in school.get('topics')
+    ]
