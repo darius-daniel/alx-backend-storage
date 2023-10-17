5a6c24587b590a128e16370dca6a7d609e994c5c
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-17 14:08:16 +0100

    Refactored query in 4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,11 @@
 -- 4. Buy buy buy
-
-DROP TRIGGER IF EXISTS after_order_insert;
-
-CREATE TRIGGER after_order_insert
+DELIMITER $$
+CREATE TRIGGER IF NOT EXISTS after_order_insert
   AFTER INSERT 
-  ON ordersFOR EACH ROW
-UPDATE items
-  SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
+  ON orders FOR EACH ROW
+BEGIN
+  UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
