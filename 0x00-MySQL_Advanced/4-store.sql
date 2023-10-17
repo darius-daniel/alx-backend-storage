5fe31c4d872079e9ddb7a60bc32361762b79a576
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-17 14:12:23 +0100

    Please work this time

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,9 @@
 -- 4. Buy buy buy
+-- Drop the trigger if it exists
+DROP TRIGGER IF EXISTS after_order_insert;
+-- Change the delimiter
 DELIMITER $$
+-- Create the trigger again
 CREATE TRIGGER IF NOT EXISTS after_order_insert
   AFTER INSERT 
   ON orders FOR EACH ROW
@@ -8,4 +12,5 @@ BEGIN
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
 END $$
+-- Change the delimiter back to the default
 DELIMITER ;
