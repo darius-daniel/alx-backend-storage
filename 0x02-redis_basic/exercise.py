702b61431b7f5343fe4ebfc565870239c0076cb1
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-18 12:38:24 +0100

    New directory for new project

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Module containing classes"""
+import redis
+import uuid
+from typing import Union, Callable, Optional
+
+
+class Cache:
+    """A Cache class"""
+    def __init__(self) -> None:
+        """
+        Initialises a new Cache instance by storing a new instance of the
+        Redis client as a private variable _redis
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Generates a random key
+        """
+        key = uuid.uuid4()
+        self._redis.mset({str(key): data})
+        return str(key)
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, int, None]:
+        """Performs some operation"""
+        if fn is None:
+            return None
+
+        return fn(self._redis.get(key))
+
+    def get_str(self, key: str):
+        return self.get(key, str)
+
+    def get_int(self, key: str):
+        return self.get(key, int)
