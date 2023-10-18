f6d1bb9b831e8e981394cfe4d4689afb87966b2d
Darius Daniel <dariusdaniel771@gmail.com>
2023-10-18 14:50:29 +0100

    Pushing code extension for task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,6 +23,24 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    Stores the history of inputs and outputs for a particular function
+    """
+    in_key = method.__qualname__ + ":inputs"
+    out_key = method.__qualname__ + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args) -> Any:
+        self._redis.rpush(in_key, str(args))
+        output = method(self, *args)
+        self._redis.rpush(out_key, str(output))
+
+        return output
+
+    return wrapper
+
+
 class Cache:
     """A Cache class"""
     def __init__(self) -> None:
@@ -34,6 +52,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Generates a random key
